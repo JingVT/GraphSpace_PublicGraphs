713a0dc7d497938fa5b490d8b439f00d2bd854d5
JingVT <33851894+JingVT@users.noreply.github.com>
2018-01-28 20:44:32 -0500

    Update tableOfUsers.js

--- tableOfUsers.js
+++ tableOfUsers.js
@@ -1,14 +1,11 @@
 
 
 // plot a table for each user who has updated at leat one graph. Data is sorted according to the number of graphs, in a .csv file 
-
 d3.csv('TotalPublicGraphs.csv', function (error,data) {
-
-
-  function tabulate(data, columns) {
+      function tabulate(data, columns) {
 		var table = d3.select('#userstable').append('table')
 		var thead = table.append('thead')
-		var	tbody = table.append('tbody');
+		var tbody = table.append('tbody');
 
 		// append the header row
 		thead.append('tr')
@@ -17,35 +14,28 @@ d3.csv('TotalPublicGraphs.csv', function (error,data) {
 		  .append('th')
 		    .text(function (column) { return column; });
 
-
 		// create a row for each object in the data
 		var rows = tbody.selectAll('tr')
 		  .data(data)
 		  .enter()
 		  .append('tr');
 
-
 		// create a cell in each row for each column
 		var cells = rows.selectAll('td')
 		  .data(function (row) {
-		    return columns.map(function (column) {
+		      return columns.map(function (column) {
 		      return {column: column, value: row[column]};
-		    });
-		  })
+		      });
+		   })
 		  .enter()
 		  .append('td')
-		    .text(function (d) { return d.value; });
+		  .text(function (d) { return d.value; });
 
-	  return table;
-
+	       return table;
 	}
 
-
-	// render the table(s)
-	tabulate(data, ['Graph Owner', 'Number of Public Graphs']); // 2 column table
-
-
-
+      // render the table(s)
+      tabulate(data, ['Graph Owner', 'Number of Public Graphs']); // 2 column table
 });
 
 
